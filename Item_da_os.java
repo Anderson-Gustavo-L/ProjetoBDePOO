c00c1a44bc53443b3b9d3b9b3499cbd2529b82b5
Anderson <anderson.gustavo.lourenco@gmail.com>
2025-12-27 13:39:47 -0300

    inuteis

--- Item_da_os.java
+++
@@ -1,8 +0,0 @@
-package Criatech;
-
-public class Item_da_os {
-    private int id_item_da_os;
-    private String tipo;
-    private String descricao;
-    private double valor_unitario;
-}
--- Ordem_de_servico.java
+++
@@ -1,8 +0,0 @@
-package Criatech;
-
-public class Ordem_de_servico extends Cliente {
-    private int id_os;
-    private String data_abertura;
-    private String status;
-
-}
